f502d782a8d6433cff74762ebd793dea99f594cb
Bertrand Thomas <bertrand@devpro.fr>
2022-11-02 11:35:19 +0100

    Fix breaking change with Withywoods.Selenium 1.5

--- dotnet/test/Api.UnitTests/Controllers/SwaggerControllerTest.cs
+++ dotnet/test/Api.UnitTests/Controllers/SwaggerControllerTest.cs
@@ -1,34 +1,32 @@
 ﻿using FluentAssertions;
 using OpenQA.Selenium;
 using Withywoods.Selenium;
-using Withywoods.WebTesting.TestHost;
+using Withywoods.WebTesting;
 using Xunit;
 
 namespace KeepTrack.Api.UnitTests.Controllers
 {
     [Trait("Category", "UnitTests")]
-    public class SwaggerControllerTest : SeleniumTestBase, IClassFixture<LocalServerFactory<Startup>>
+    public class SwaggerControllerTest : SeleniumTestBase, IClassFixture<WebApplicationFactoryFixture<Startup>>
     {
         private const string ResourceEndpoint = "swagger";
 
-        private readonly LocalServerFactory<Startup> _server;
+        private readonly string _webUrl = "https://localhost:5001";
 
-        public SwaggerControllerTest(LocalServerFactory<Startup> server)
+        public SwaggerControllerTest(WebApplicationFactoryFixture<Startup> factory)
             : base(new WebDriverOptions { IsHeadless = true })
         {
-            _server = server;
-            _ = server.CreateClient(); // needed to change server state
+            factory.HostUrl = _webUrl;
+            factory.CreateDefaultClient();
         }
 
         [Fact]
         public void AspNetCoreApiSampleSwaggerResourceGet_ReturnsHttpOk()
         {
-            _server.RootUri.Should().Be("https://localhost:5001");
-
             try
             {
                 // Arrange & Act
-                WebDriver.Navigate().GoToUrl($"{_server.RootUri}/{ResourceEndpoint}");
+                WebDriver.Navigate().GoToUrl($"{_webUrl}/{ResourceEndpoint}");
 
                 // Assert
                 WebDriver.FindElement(By.ClassName("title"), 60);
