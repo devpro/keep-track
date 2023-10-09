043fbcb65a88358434b6d4d0d416bf5c38050a4a
Bertrand Thomas <bertrand@devpro.fr>
2023-10-09 11:51:32 +0200

    Fix migration update for http module

--- angular-bootstrap/src/app/user/services/jwt-interceptor.service.ts
+++ angular-bootstrap/src/app/user/services/jwt-interceptor.service.ts
@@ -1,5 +1,5 @@
+import { HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
