9881de13c2be08454bc53ac3a8ae675e3fa61dc7
Bertrand Thomas <bertrand@devpro.fr>
2023-10-09 11:26:10 +0200

    Migrate angular app to 15
    Result of command `ng update @angular/cli@15 @angular/core@15 @angular-eslint/schematics@15 @angular/fire@7.5`

--- angular-bootstrap/src/app/app-routing.module.ts
+++ angular-bootstrap/src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })
+    RouterModule.forRoot(routes, {})
   ],
   exports: [
     RouterModule
